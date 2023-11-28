1b0ec4ca8a55c256d6fd7d9dfb93db5a2121d69c
Paulo Sergio <pscosta2008@gmail.com>
2023-11-28 16:59:48 -0300

    feat: header nao responsivo finalizado

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function abrirDropDownHeaderPerfil(){
+
+    const dropDownContainer = document.getElementById("dropDown-perfil-icon")
+    dropDownContainer.style.display = 'block'
+    console.log('a')
+
+}
