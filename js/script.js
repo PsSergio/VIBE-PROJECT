ec73a09c83446e75280c4c3b7ddfe428215d1fc3
Paulo Sergio <pscosta2008@gmail.com>
2023-11-29 00:16:41 -0300

    feat: Header responsivo finalizado

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-function abrirDropDownHeaderPerfil(){
+let dropDownOpened = false;
 
-    const dropDownContainer = document.getElementById("dropDown-perfil-icon")
-    dropDownContainer.style.display = 'block'
-    console.log('a')
+function abrirDropDown(dropDownContainer){
+
+    if(!dropDownOpened){
+        dropDownContainer.style.display = 'block'
+        dropDownOpened = true
+        // console.log('a')
+        return
+    }
+    dropDownContainer.style.display = 'none'
+    dropDownOpened = false;
+    // console.log('b')
+
+    
+}
+
+function closeDropDown(dropDown){
+
+    dropDown.style.display = 'none'
 
 }
