30cc6a25b0cfae67e052e4b915557f88bd5eb3ac
Paulo Sergio <pscosta2008@gmail.com>
2023-11-30 21:04:22 -0300

    feat: Melhorando desing do footer

--- js/script.js
+++ js/script.js
@@ -20,3 +20,18 @@ function closeDropDown(dropDown){
     dropDown.style.display = 'none'
 
 }
+
+const subtitlesFooter = document.getElementsByClassName("subtitle-content")
+// const descFooter = document.getElementsByClassName("desc-content")
+
+for(let i = 0; i < subtitlesFooter.length; i++){
+
+    subtitlesFooter[i].addEventListener("click", (e) => {
+
+        const descContent = e.target.parentNode.children[1]
+        
+        abrirDropDown(descContent)
+
+    })
+
+}
