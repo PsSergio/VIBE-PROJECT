c812509f7aeb0efdc3b1e8fab75364bfa535d1c3
Paulo Sergio <pscosta2008@gmail.com>
2023-12-11 17:24:14 -0300

    feat: Consumindo API para pegar produtos do carrossel

--- js/apiCarrossel.js
+++ js/apiCarrossel.js
@@ -1,29 +1,63 @@
-async function getAPIforCarrossel(id){
+async function getAPIforCarrossel(i, id, carrossel){
 
     return await fetch(URL_ACHAR_PRODUTOS+`?codigoProduto=${id}`).then(response => {
         response.json().then(produto =>{
-            
-            document.getElementsByClassName("name-product-page")[0].innerHTML = produto.descProduto
-            document.getElementsByClassName("subDesc-product-page")[0].innerHTML = produto.subDescProduto
-            document.getElementsByClassName("price-product-page")[0].innerHTML = "R$" + produto.precoProduto
 
-            //images 
-
-            document.getElementsByClassName("img-product-general")[0].src = produto.images.front
-            document.getElementsByClassName("img-product-general")[1].src = produto.images.back
-            document.getElementsByClassName("img-product-general")[2].src = produto.images.sideLeft
-            document.getElementsByClassName("img-product-general")[3].src = produto.images.sideRigth
-            document.getElementsByClassName("img-product-general")[4].src = produto.images.top
-            document.getElementsByClassName("img-product-general")[5].src = produto.images.front
-            document.getElementsByClassName("img-product-general")[6].src = produto.images.back
-            document.getElementsByClassName("img-product-general")[7].src = produto.images.sideLeft
-            document.getElementsByClassName("img-product-general")[8].src = produto.images.sideRigth
-            document.getElementsByClassName("img-product-general")[9].src = produto.images.top
-
-            idPacoteAtual = produto.pacoteProduto.idPacote
+            trocaElementosCarrossel(i, carrossel, produto.images.front, produto.descProduto, produto.precoProduto)
 
         })
 
     })
 
 }
+
+
+function sortearNumeros(){
+    
+    let listaAleatório = []
+    for(let i = 0; i < 9; i++){
+
+        let numGerado = Math.floor(Math.random() * 16)+1
+        if(listaAleatório.indexOf(numGerado) == -1){
+
+            listaAleatório.push(numGerado)
+
+        }else{
+            i--
+        }
+
+    }
+
+    return listaAleatório
+
+}
+
+function trocaElementosCarrossel(i, carrossel, img, name, price){
+
+        const nameEl = carrossel.children[i].children[1]
+        const imgEl = carrossel.children[i].children[0]
+        const priceEl = carrossel.children[i].children[2]
+
+        nameEl.innerHTML = name
+        imgEl.src = img
+        priceEl.innerHTML = 'R$'+price
+
+
+}
+
+// trocaElementosCarrossel(document.getElementsByClassName("carousel")[0])
+
+function executaTrocaDosElementosCarrossel(){
+
+    listaIdSorteados = sortearNumeros()
+    // console.log(listaIdSorteados)
+
+    for(let i = 0; i < 9; i++){
+
+        getAPIforCarrossel(i, listaIdSorteados[i], document.getElementById("carousel-pag-produto"))
+
+    }
+
+}
+
+executaTrocaDosElementosCarrossel()
